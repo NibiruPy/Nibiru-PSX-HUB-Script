44ca2508edb73f35d9deeae24d7bb5801a78a703
Nibiru <127780084+VV-Nibiru-VV@users.noreply.github.com>
2023-03-13 19:31:40 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,15 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
+local Window = OrionLib:MakeWindow({Name = "Nibiru [PSX-HUB]", HidePremium = false, SaveConfig = true, ConfigFolder = "Nibiru"})
+OrionLib:MakeNotification({
+	Name = "Title!",
+	Content = "Welcome to Nibiru [PSX-HUB]",
+	Image = "rbxassetid://4483345998",
+	Time = 10
+})
+
+local Tab = Window:MakeTab({
+	Name = "PetChangerID",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
