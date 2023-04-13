da15d5136b8cb3d33bfe23a6b3d803b28691d577
Nibiru <127780084+NibiruPy@users.noreply.github.com>
2023-04-13 18:32:46 +0200

    Update script2.lua

--- script2.lua
+++ script2.lua
@@ -1,9 +1,9 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Nibiru [PSX-HUB]", HidePremium = false, SaveConfig = true, ConfigFolder = "Nibiru"})
+local Window = OrionLib:MakeWindow({Name = "PSX [YT_Zefko-HUB]", HidePremium = false, SaveConfig = true, ConfigFolder = "YT_Zefko"})
 OrionLib:MakeNotification({
-	Name = "Nibiru [PSX-HUB]",
-	Content = "Welcome to Nibiru [PSX-HUB]",
+	Name = "PSX [YT_Zefko-HUB]",
+	Content = "Welcome to PSX [YT_Zefko-HUB]",
 	Image = "rbxassetid://4483345998",
 	Time = 10
 })
@@ -15,7 +15,7 @@ local Tab = Window:MakeTab({
 })
 
 Tab:AddButton({
-	Name = "Press [ON]",
+	Name = "Activate",
 	Callback = function()
       		print("button pressed")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ReturnString9999/P-S-X-main-lulz/main/Changer/.lua"))()
