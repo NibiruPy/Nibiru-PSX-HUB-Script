91ee236264a4ce2e0a00f8cfad95ae7731a29eda
YT_Zefko <127780084+Zefko777@users.noreply.github.com>
2023-05-01 11:22:24 +0200

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -1,9 +1,9 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "PSX-HUB by [YT_Zefko-HUB]", HidePremium = false, SaveConfig = true, ConfigFolder = "YT_Zefko"})
+local Window = OrionLib:MakeWindow({Name = "PSX-HUB by [YT_Zefko-HUB] | FREE VERSION", HidePremium = false, SaveConfig = true, ConfigFolder = "YT_Zefko"})
 OrionLib:MakeNotification({
 	Name = "PSX-HUB by [YT_Zefko-HUB]",
-	Content = "Welcome to PSX-HUB by [YT_Zefko-HUB]",
+	Content = "Welcome to PSX-HUB by [YT_Zefko-HUB] | FREE VERSION",
 	Image = "rbxassetid://4483345998",
 	Time = 10
 })
